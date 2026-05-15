dc2d5a83b354153b9076267e9309998c689df4cd
dwmusoke <dwmusoke@gmail.com>
2026-05-15 19:03:53 +0300

    Fix client-side crash on new modules: remove null-loader webpack rule that blanked out page chunks

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -3,16 +3,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config, { isServer }) => {
-    // Ignore specific modules that cause build errors
-    if (!isServer) {
-      config.module.rules.push({
-        test: /(agency-insights|daily-sales|fare-optimization|iata-tracking|unused-tickets)/,
-        use: 'null-loader',
-      });
-    }
-    return config;
-  },
+
 };
 
 module.exports = nextConfig;
